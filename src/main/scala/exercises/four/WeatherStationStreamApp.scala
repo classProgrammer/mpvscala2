bdd83a00d500b50ef826c03da4fb746b1898990c
classProgrammer <gerald.application@gmail.com>
2020-01-02 17:31:55 +0100

    corrected 2.4

--- src/main/scala/exercises/four/WeatherStationStreamApp.scala
+++ src/main/scala/exercises/four/WeatherStationStreamApp.scala
@@ -3,7 +3,7 @@ package exercises.four
 import java.nio.file.{Path, Paths}
 import java.nio.file.StandardOpenOption.{APPEND, WRITE}
 
-import akka.NotUsed
+import akka.{Done, NotUsed}
 import akka.actor.{ActorSystem, Cancellable}
 import akka.stream.IOResult
 import akka.util.ByteString
@@ -17,23 +17,22 @@ import scala.concurrent.duration._
 object WeatherStationTypes {
   type Measurement = (String, (String, Float, TemperatureUnit))
   type SourceType = Source[Measurement, Cancellable]
-  type FlowType = Flow[Measurement, ByteString, NotUsed]
+  type FlowTypeSeq = Flow[Measurement, ByteString, NotUsed]
+  type FlowType = Flow[Measurement, IOResult, NotUsed]
   type SinkType = Sink[ByteString, Future[IOResult]]
 }
 
 object WeatherStationConfig {
-  val fileName = "streamed_weather_station.txt"
-  val file = Paths.get(s"./src/main/scala/exercises/four/file/$fileName")
-  val batchSize = 7
-  val elements = 100
-  val elementsToThrottle = 1
+  val basePath = "./src/main/scala/exercises/four/file"
+  val bulkWriteSize = 7
+  val bulkWriteDelay = 100.millis
+  val elementsToGenerate = 100
+  val elementsToThrottle = bulkWriteSize
   val parallelism = 4
-  val generators = Seq(
-    new MeasurementGenerator(20.2f, 22.15f, TemperatureUnit.Celsius),
-    new MeasurementGenerator(22.55f, 24.12f, TemperatureUnit.Celsius),
-    new MeasurementGenerator(18.45f, 19.68f, TemperatureUnit.Celsius),
-    new MeasurementGenerator(40.77f, 52.93f, TemperatureUnit.Fahrenheit)
-  )
+  val waitForGrouping: FiniteDuration = 200.millis
+  val maxGenerationDelay = 10
+  val minGenerationDelay = 1
+  val diffGenerationDelay = maxGenerationDelay - minGenerationDelay
 }
 
 object WeatherStationStreamApp extends App {
@@ -50,15 +49,119 @@ object WeatherStationStreamApp extends App {
     Future { Console.println(s"$msg") }
   }
 
-  private def getFlow(): FlowType = {
+  // ======================== SOURCE ========================
+  private def getSource(name: String, gen: MeasurementGenerator, interval: FiniteDuration): SourceType = {
+    var number = 1
+    Source.tick(interval, interval, 1).map(_ => {
+      val measurement = gen.getMeasurement()
+      println(s"$name:$number => $measurement")
+      number += 1
+      (name, measurement)
+    })
+  }
+
+  // ======================== FLOWS ========================
+  private def getFlow(): FlowTypeSeq = {
     Flow[Measurement].map(x => {
       println(s"+++ write element")
       measurementAsByteStringLine(x)
     })
   }
 
+  private def getFlow(delay: FiniteDuration): FlowTypeSeq = {
+    Flow[Measurement]
+      .throttle(elementsToThrottle, delay)
+      .map(x => {
+        println(s"+++ write element")
+        measurementAsByteStringLine(x)
+    })
+  }
+
+  def getChunkedFlow(batchSize: Int, delay: FiniteDuration): FlowTypeSeq = {
+    Flow[Measurement]
+      .throttle(elementsToThrottle, delay)
+      .groupedWithin(batchSize, waitForGrouping)
+      .map(collection => {
+        println(s"--- write ${collection.size} elements")
+        measurementSequenceAsByteStringLine(collection)
+      })
+  }
+
+  def getFlowMapAsync(delay: FiniteDuration, sink: SinkType): FlowType = {
+    Flow[Measurement]
+      .throttle(elementsToThrottle, delay)
+      .mapAsync(parallelism)(elem => {
+        println(s"--- write to file")
+        Source(Seq(elem)).map(measurementAsByteStringLine).runWith(sink)
+      })
+  }
+
+  def getChunkedFlowMapAsync(batchSize: Int, sink: SinkType): FlowType = {
+    Flow[Measurement]
+      .groupedWithin(batchSize, waitForGrouping)
+      .mapAsync(parallelism)(collection => {
+        println(s"--- write ${collection.size} elements")
+        Source(collection).map(measurementAsByteStringLine).runWith(sink)
+      })
+  }
+
+  def getChunkedFlowMapAsync(batchSize: Int, delay: FiniteDuration, sink: SinkType): FlowType = {
+    Flow[Measurement]
+      .throttle(elementsToThrottle, delay)
+      .groupedWithin(batchSize, waitForGrouping)
+      .mapAsync(parallelism)(collection => {
+        println(s"--- write ${collection.size} elements")
+        Source(collection).map(measurementAsByteStringLine).runWith(sink)
+      })
+  }
+
+  def getChunkedFlowMapAsyncRoundRobin(batchSize: Int, delay: FiniteDuration, sinks: Seq[SinkType]): FlowType = {
+    var idx = 0
+    val n = sinks.size
+
+    Flow[Measurement]
+      .throttle(elementsToThrottle, delay)
+      .groupedWithin(batchSize, waitForGrouping)
+      .map(collection => {
+        val tuple = (collection, sinks(idx))
+        idx = (idx + 1) % n
+        tuple
+      })
+      .mapAsync(parallelism)(tuple => {
+        println(s"--- write ${tuple._1.size} elements")
+        Source(tuple._1).map(measurementAsByteStringLine).runWith(tuple._2)
+      })
+  }
+
+  // ======================== SINKS ========================
+  private def getSink(path: Path): SinkType = {
+    FileIO.toPath(path, Set(WRITE, APPEND))
+  }
+
+  // ======================== PIPELINES ========================
+  private def pipeline(source: SourceType, flow: FlowTypeSeq, sink: SinkType, elems: Int): Future[IOResult] = {
+    source limit (elems - 1) via flow runWith sink
+  }
+
+  private def pipeline(source: SourceType, flow: FlowTypeSeq, sink: SinkType): Future[IOResult] = {
+    source via flow runWith sink
+  }
+
+  private def pipeline(source: SourceType, flow: FlowType, elems: Int): Future[Done] = {
+    source limit (elems - 1) via flow runWith Sink.ignore
+  }
+
+  private def pipeline(source: SourceType, flow: FlowType): Future[Done] = {
+    source via flow runWith Sink.ignore
+  }
+
+  // ======================== MEASUREMENT TO (BYTE)STRING ========================
   private def measurementAsByteStringLine(measurement: Measurement): ByteString = {
     ByteString(s"${measurementAsString(measurement)}\n")
+  }
+
+  private def measurementSequenceAsByteStringLine(measurements: Seq[Measurement]): ByteString = {
+    ByteString(measurements.map(measurementAsStringLine).mkString(""))
   }
 
   private def measurementAsString(measurement: Measurement) = {
@@ -69,94 +172,35 @@ object WeatherStationStreamApp extends App {
     s"${measurementAsString(measurement)}\n"
   }
 
-  private def getSink(path: Path): SinkType = {
-    FileIO.toPath(path, Set(WRITE, APPEND))
-  }
-
-  private def getFlow(delay: FiniteDuration): FlowType = {
-    Flow[Measurement].throttle(elementsToThrottle, delay).map(x => {
-      println(s"+++ write element")
-      measurementAsByteStringLine(x)
-    })
-  }
-
-  private def pipeline(source: SourceType, flow: FlowType, sink: SinkType, elems: Int): Future[IOResult] = {
-    source limit (elems - 1) via flow runWith sink
-  }
-
-  private def pipeline(source: SourceType, flow: FlowType, sink: SinkType): Future[IOResult] = {
-    source via flow runWith sink
-  }
-
-  def makeCollection(collection: Seq[Measurement], elem: Measurement): Seq[Measurement] = {
-    collection :+ elem
-  }
-
-  def getChunkedFlowMapAsync(batchSize: Int): FlowType = {
-    Flow[Measurement].batch(batchSize, Seq(_)) (makeCollection)
-      .mapAsync(parallelism)(collection => Future {
-        println(s"--- Write ${collection.size} items to file")
-        ByteString(collection.map(measurementAsStringLine).mkString("\n"))
-    })
-  }
-
-  def getChunkedFlowMapAsync(batchSize: Int, delay: FiniteDuration): FlowType = {
-    Flow[Measurement].batch(batchSize, Seq(_)) (makeCollection)
-      .throttle(elementsToThrottle, delay)
-      .mapAsync(parallelism)(collection => Future {
-        println(s"--- Write ${collection.size} items to file")
-        ByteString(collection.map(measurementAsStringLine).mkString(""))
-    })
-  }
-
-  def getChunkedFlowMapAsync(delay: FiniteDuration): FlowType = {
-    Flow[Measurement]
-      .throttle(elementsToThrottle, delay)
-      .mapAsync(parallelism)(elem => Future {
-        println(s"--- write to file")
-        measurementAsByteStringLine(elem)
-      })
-  }
-
-  def getChunkedFlow(batchSize: Int, delay: FiniteDuration): FlowType = {
-    Flow[Measurement]
-      .throttle(elementsToThrottle, delay)
-      .map(elem => {
-        println(s"--- write to file")
-        measurementAsByteStringLine(elem)
-      })
-  }
-
+  // ======================== PROGRAM ========================
   def main(): Unit = {
     println("========== WeatherStationStream App ==========")
+    val rand = scala.util.Random
 
-    val sources = Seq(
-      getSource("Linz", generators(0), 2.millis),
-      getSource("Vienna", generators(1), 3.millis),
-      getSource("Dornbirn", generators(2), 5.millis),
-      getSource("Las Vegas", generators(3), 7.millis)
+    val fileNames = Seq(
+      "streamed_weather_station1.txt",
+      "streamed_weather_station2.txt"
+    )
+    val files = fileNames.map(fileName => Paths.get(s"$basePath/$fileName"))
+    val sinks = files.map(getSink)
+
+    val sourceNames = Seq(
+      "Linz",
+      "Vienna",
+      "Dornbirn",
+      "New York"
     )
 
-    val flow = getChunkedFlowMapAsync(batchSize, 200.millis)
-    val sink = getSink(file)
+    val generators = sourceNames.map(_ => new MeasurementGenerator(20.2f, 24.15f, TemperatureUnit.Celsius))
+    val sources = (0 to generators.size - 1).map(i => getSource(sourceNames(i), generators(i), (minGenerationDelay + rand.nextInt(diffGenerationDelay) + 1).millis))
 
-    val futures: Future[Seq[IOResult]] = Future.sequence(
-      sources.map(source => pipeline(source, flow, sink, elements)))
+    val flow = getChunkedFlowMapAsyncRoundRobin(bulkWriteSize, bulkWriteDelay, sinks)
+    val futures = Future.sequence(sources.map(source => pipeline(source, flow, elementsToGenerate)))
 
     Await.ready(futures, Duration.Inf)
     println("========== END WeatherStationStream App ==========")
-    Thread.sleep(1000)
+    Thread.sleep(100)
     system.terminate()
-  }
-
-  private def getSource(name: String, gen: MeasurementGenerator, interval: FiniteDuration): SourceType = {
-    var number = 1
-    Source.tick(interval, interval, 1).map(_ => {
-      val m = gen.getMeasurement()
-      println(s"$name:$number => $m")
-      number += 1
-      (name, m)
-    })
   }
 
   main()
